9b005bcf9f494c7e78a24e9cab1a43b7e4ec666b
Shawn Cicoria <github@cicoria.com>
2020-12-21 16:40:03 +0000

    script updates

--- src/run2_model.sh
+++ src/run2_model.sh
@@ -5,16 +5,6 @@ BSPLIT=4
 EVALEVERY=100
 
 EX_PER_CLASS=5
-for S in 42 7 21 58 99
-do
-  python -m bit_pytorch.train --name cifar10_1_class_$(date +%F_%H%M%S) \
-    --model BiT-M-R50x3 --datadir ../data --logdir ../logs --dataset cifar10 \
-    --bit_pretrained_dir ../models --batch_split $BSPLIT --eval_every $EVALEVERY \
-    --examples_per_class_seed $S --examples_per_class 1 --save 
-done
-
-
-EX_PER_CLASS=5
 for S in 55 2 12 85 22
 do
   python -m bit_pytorch.train --name cifar10_5_class_$(date +%F_%H%M%S) \
