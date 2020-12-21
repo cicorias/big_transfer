a178f407df9e89ef685209d7d41f4de71244d688
Shawn Cicoria <github@cicoria.com>
2020-12-21 15:29:13 +0000

    batch it

--- src/run_model.sh
+++ src/run_model.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 export PYTHONPATH="$(dirname $(pwd))"
 echo "pypath=$PYTHONPATH"
-BSPLIT=8
+BSPLIT=4
+EVALEVERY=100
+
+EX_PER_CLASS=5
 for S in 42 7 21 58 99
 do
   python -m bit_pytorch.train --name cifar10_$(date +%F_%H%M%S) \
     --model BiT-M-R50x3 --datadir ../data --logdir ../logs --dataset cifar10 \
-    --bit_pretrained_dir ../models --batch_split $BSPLIT --eval_every 4 \
+    --bit_pretrained_dir ../models --batch_split $BSPLIT --eval_every $EVALEVERY \
     --examples_per_class_seed $S --examples_per_class 1 --save 
+done
+
+
+EX_PER_CLASS=5
+for S in 42 7 21 58 99
+do
+  python -m bit_pytorch.train --name cifar10_$(date +%F_%H%M%S) \
+    --model BiT-M-R50x3 --datadir ../data --logdir ../logs --dataset cifar10 \
+    --bit_pretrained_dir ../models --batch_split $BSPLIT --eval_every $EVALEVERY \
+    --examples_per_class_seed $S --examples_per_class $EX_PER_CLASS --save 
 done
